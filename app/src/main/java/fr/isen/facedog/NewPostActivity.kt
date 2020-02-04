1decca9764df09e93d3d66662d05b0c4ebf9ae06
Morgane Gougeon <gougeon.morgane@gmail.com>
2020-02-04 17:31:05 +0100

    Add bottomNavBar listeners

---
+++ app/src/main/java/fr/isen/facedog/NewPostActivity.kt
@@ -0,0 +1,34 @@
+package fr.isen.facedog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+
+class NewPostActivity : AppCompatActivity() {
+
+    lateinit var database: DatabaseReference
+    lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_post)
+        //authentication part
+        auth = FirebaseAuth.getInstance()
+
+        //database part
+        addToDatabase(database)
+
+    }
+
+    fun addToDatabase(firebaseData: DatabaseReference) {
+
+        auth.currentUser?.uid
+
+
+        val newPublication = Publication("2", "Test 3", "Antoiiiiiiiiine", auth.currentUser?.uid)
+        val key = firebaseData.child("publication").push().key ?: ""
+        newPublication.publication_id = key
+        firebaseData.child("publication").child(key).setValue(newPublication)
+    }
+}
